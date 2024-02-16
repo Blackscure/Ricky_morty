b15c691097c625bf0129a1bc4405c546946e3d25
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-16 15:38:16 +0300

    container modified to box

--- src/sections/locations/view/location.jsx
+++ src/sections/locations/view/location.jsx
@@ -1,158 +1,181 @@
+import { useState } from 'react';
+
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
+import Table from '@mui/material/Table';
 import Button from '@mui/material/Button';
-import CardMedia from '@mui/material/CardMedia';
+import TableBody from '@mui/material/TableBody';
 import Typography from '@mui/material/Typography';
-import CardContent from '@mui/material/CardContent';
+import TableContainer from '@mui/material/TableContainer';
+import TablePagination from '@mui/material/TablePagination';
+
+import { useRouter } from 'src/routes/hooks';
+
+import { users } from 'src/_mock/user';
 
 import Iconify from 'src/components/iconify';
+import Scrollbar from 'src/components/scrollbar';
 
-const character = {
-    
-        "id": 1,
-        "name": "Rick Sanchez",
-        "status": "Alive",
-        "species": "Human",
-        "type": "",
-        "gender": "Male",
-        "origin": {
-            "name": "Earth (C-137)",
-            "url": "https://rickandmortyapi.com/api/location/1"
-        },
-        "location": {
-            "name": "Citadel of Ricks",
-            "url": "https://rickandmortyapi.com/api/location/3"
-        },
-        "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-        "episode": [
-            "https://rickandmortyapi.com/api/episode/1",
-            "https://rickandmortyapi.com/api/episode/2",
-            "https://rickandmortyapi.com/api/episode/3",
-            "https://rickandmortyapi.com/api/episode/4",
-            "https://rickandmortyapi.com/api/episode/5",
-            "https://rickandmortyapi.com/api/episode/6",
-            "https://rickandmortyapi.com/api/episode/7",
-            "https://rickandmortyapi.com/api/episode/8",
-            "https://rickandmortyapi.com/api/episode/9",
-            "https://rickandmortyapi.com/api/episode/10",
-            "https://rickandmortyapi.com/api/episode/11",
-            "https://rickandmortyapi.com/api/episode/12",
-            "https://rickandmortyapi.com/api/episode/13",
-            "https://rickandmortyapi.com/api/episode/14",
-            "https://rickandmortyapi.com/api/episode/15",
-            "https://rickandmortyapi.com/api/episode/16",
-            "https://rickandmortyapi.com/api/episode/17",
-            "https://rickandmortyapi.com/api/episode/18",
-            "https://rickandmortyapi.com/api/episode/19",
-            "https://rickandmortyapi.com/api/episode/20",
-            "https://rickandmortyapi.com/api/episode/21",
-            "https://rickandmortyapi.com/api/episode/22",
-            "https://rickandmortyapi.com/api/episode/23",
-            "https://rickandmortyapi.com/api/episode/24",
-            "https://rickandmortyapi.com/api/episode/25",
-            "https://rickandmortyapi.com/api/episode/26",
-            "https://rickandmortyapi.com/api/episode/27",
-            "https://rickandmortyapi.com/api/episode/28",
-            "https://rickandmortyapi.com/api/episode/29",
-            "https://rickandmortyapi.com/api/episode/30",
-            "https://rickandmortyapi.com/api/episode/31",
-            "https://rickandmortyapi.com/api/episode/32",
-            "https://rickandmortyapi.com/api/episode/33",
-            "https://rickandmortyapi.com/api/episode/34",
-            "https://rickandmortyapi.com/api/episode/35",
-            "https://rickandmortyapi.com/api/episode/36",
-            "https://rickandmortyapi.com/api/episode/37",
-            "https://rickandmortyapi.com/api/episode/38",
-            "https://rickandmortyapi.com/api/episode/39",
-            "https://rickandmortyapi.com/api/episode/40",
-            "https://rickandmortyapi.com/api/episode/41",
-            "https://rickandmortyapi.com/api/episode/42",
-            "https://rickandmortyapi.com/api/episode/43",
-            "https://rickandmortyapi.com/api/episode/44",
-            "https://rickandmortyapi.com/api/episode/45",
-            "https://rickandmortyapi.com/api/episode/46",
-            "https://rickandmortyapi.com/api/episode/47",
-            "https://rickandmortyapi.com/api/episode/48",
-            "https://rickandmortyapi.com/api/episode/49",
-            "https://rickandmortyapi.com/api/episode/50",
-            "https://rickandmortyapi.com/api/episode/51"
-        ],
-        "url": "https://rickandmortyapi.com/api/character/1",
-        "created": "2017-11-04T18:48:46.250Z"
-    
-}
+import TableNoData from '../table-no-data';
+import UserTableRow from '../location-table-row';
+import TableEmptyRows from '../table-empty-rows';
+import UserTableHead from '../location-table-head';
+import UserTableToolbar from '../location-table-toolbar';
+import { emptyRows, applyFilter, getComparator } from '../utils';
 
-const LocationPage = () => {
-  // eslint-disable-next-line no-unused-vars
+// ----------------------------------------------------------------------
 
-  const handleOpenRole = () => {
-   
+export default function LocationPage() {
+  const [page, setPage] = useState(0);
+
+  const [order, setOrder] = useState('asc');
+
+  const [selected, setSelected] = useState([]);
+
+  const [orderBy, setOrderBy] = useState('name');
+
+  const [filterName, setFilterName] = useState('');
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const router = useRouter();
+
+  const handleSort = (event, id) => {
+    const isAsc = orderBy === id && order === 'asc';
+    if (id !== '') {
+      setOrder(isAsc ? 'desc' : 'asc');
+      setOrderBy(id);
+    }
   };
 
- 
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = users.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPage(0);
+    setRowsPerPage(parseInt(event.target.value, 10));
+  };
+
+  const handleFilterByName = (event) => {
+    setPage(0);
+    setFilterName(event.target.value);
+  };
+
+  const dataFiltered = applyFilter({
+    inputData: users,
+    comparator: getComparator(order, orderBy),
+    filterName,
+  });
+
+  const notFound = !dataFiltered.length && !!filterName;
+
+  const goToCreateUser = () => {
+    router.push('/create-user');
+  }
 
   return (
     <Box>
       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-        <Typography variant="h4">Create Users</Typography>
+        <Typography variant="h4">Users</Typography>
 
-        <Button variant="contained" onClick={handleOpenRole} color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
-          Add Role
+        <Button variant="contained" onClick={goToCreateUser} color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
+          New User
         </Button>
       </Stack>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt={character.name}
-        height="100%"
-        image={character.image}
-      />
-      <CardContent>
-        <Typography variant="h6" component="div">
-          {character.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          ID: {character.id}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Status: {character.status}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Species: {character.species}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Type: {character.type || 'N/A'}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Gender: {character.gender}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Origin: {character.origin.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Location: {character.location.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Episodes:
-          <ul>
-            {character.episode.map((episode, index) => (
-              <li key={index}>
-                <a href={episode}>{episode}</a>
-              </li>
-            ))}
-          </ul>
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          URL: <a href={character.url}>{character.url}</a>
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Created: {character.created}
-        </Typography>
-      </CardContent>
-    </Card>
+
+      <Card>
+        <UserTableToolbar
+          numSelected={selected.length}
+          filterName={filterName}
+          onFilterName={handleFilterByName}
+        />
+
+        <Scrollbar>
+          <TableContainer sx={{ overflow: 'unset' }}>
+            <Table sx={{ minWidth: 800 }}>
+              <UserTableHead
+                order={order}
+                orderBy={orderBy}
+                rowCount={users.length}
+                numSelected={selected.length}
+                onRequestSort={handleSort}
+                onSelectAllClick={handleSelectAllClick}
+                headLabel={[
+                  { id: 'name', label: 'Name' },
+                  { id: 'company', label: 'Company' },
+                  { id: 'role', label: 'Role' },
+                  { id: 'isVerified', label: 'Verified', align: 'center' },
+                  { id: 'status', label: 'Status' },
+                  { id: '' },
+                ]}
+              />
+              <TableBody>
+                {dataFiltered
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => (
+                    <UserTableRow
+                      key={row.id}
+                      name={row.name}
+                      role={row.role}
+                      status={row.status}
+                      company={row.company}
+                      avatarUrl={row.avatarUrl}
+                      isVerified={row.isVerified}
+                      selected={selected.indexOf(row.name) !== -1}
+                      handleClick={(event) => handleClick(event, row.name)}
+                    />
+                  ))}
+
+                <TableEmptyRows
+                  height={77}
+                  emptyRows={emptyRows(page, rowsPerPage, users.length)}
+                />
+
+                {notFound && <TableNoData query={filterName} />}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Scrollbar>
+
+        <TablePagination
+          page={page}
+          component="div"
+          count={users.length}
+          rowsPerPage={rowsPerPage}
+          onPageChange={handleChangePage}
+          rowsPerPageOptions={[5, 10, 25]}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Card>
     </Box>
   );
-};
-
-export default LocationPage ;
+}
