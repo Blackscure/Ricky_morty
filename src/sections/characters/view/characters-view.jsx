31814c141a3343752fec567dff1b7ee38be93e19
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-17 08:47:12 +0300

    container modified to Box

--- src/sections/characters/view/characters-view.jsx
+++ src/sections/characters/view/characters-view.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 
+import { Box } from '@mui/material';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Table from '@mui/material/Table';
 import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
 import TableBody from '@mui/material/TableBody';
 import Typography from '@mui/material/Typography';
 import TableContainer from '@mui/material/TableContainer';
@@ -18,12 +18,11 @@ import Iconify from 'src/components/iconify';
 import Scrollbar from 'src/components/scrollbar';
 
 import TableNoData from '../table-no-data';
+import TableEmptyRows from '../table-empty-rows';
 import UserTableRow from '../character-table-row';
 import UserTableHead from '../character-table-head';
-import TableEmptyRows from '../table-empty-rows';
 import UserTableToolbar from '../character-table-toolbar';
 import { emptyRows, applyFilter, getComparator } from '../utils';
-import { Box } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
